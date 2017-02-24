b114f9932c0dfa61c96f64533101a4953ac7e841
Riyang Liu <riyangl@student.ucsd.edu>
2017-02-23 19:55:03 -0800

    fixing

--- routes/closet.js
+++ routes/closet.js
@@ -1,14 +1,4 @@
 var closetdata = require('../closetdata.json');
-<<<<<<< HEAD
-var clothes;
-var fs = require('fs');
-
-exports.view = function(req, res){
-  res.render('closet', closetdata);
-};
-
-=======
-
 
 exports.view = function(req, res){
 
