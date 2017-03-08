e3226a9bb229d21904984c260b675e2a168021c5
Jingya Huang <jih201@ucsd.edu>
2017-03-07 18:05:02 -0800

    add closet click in google analytics

--- routes/closet.js
+++ routes/closet.js
@@ -2,13 +2,19 @@ var closetdata = require('../closetdata.json');
 var clothes;
 var fs = require('fs');
 
+/*
 exports.view = function(req, res){
   res.render('closet', closetdata);
 };
+*/
 
 exports.view = function(req, res){
 
 	res.render('closet', closetdata);
-  	
+	// the trigger for google analytics 
+	ga('create', 'UA-92899008-1', 'auto');
+	ga('send', 'pageview');
+	ga('send','event','closet','click');
+
 };
 
