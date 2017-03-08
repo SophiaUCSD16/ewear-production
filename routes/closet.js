58b22b9c42a0f5716692d62ac6b9feac035fb453
Riyang Liu <riyangl@student.ucsd.edu>
2017-03-07 19:37:45 -0800

    fixing sign up and closet.js

--- routes/closet.js
+++ routes/closet.js
@@ -12,9 +12,6 @@ exports.view = function(req, res){
 
 	res.render('closet', closetdata);
 	// the trigger for google analytics 
-	ga('create', 'UA-92899008-1', 'auto');
-	ga('send', 'pageview');
-	ga('send','event','closet','click');
 
 };
 
